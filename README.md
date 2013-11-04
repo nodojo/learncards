a415d6abe0de47de5b203a12794357dac1b677e7
brettlangdon <brett@blangdon.com>
2013-11-03 21:55:51 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+learncards
+==========
+
+Learn Cards is a simple module which allows you to host a web server
+of custom defined flash cards.
+
+Simply install `learncards` and provide a simple json file defining
+all of the questions and answers for the flash cards.
+
+Why did I pick a stupid name like `learncards`? Because someone already
+took the name `flashcard` in npm and using `flashcards` seems confusing.
+So in come my limited creativity when it comes to naming projects.
+
+## Installing
+### Via Git
+```bash
+git clone git://github.com/brettlangdon/learncards.git
+cd ./learncards
+npm install
+
+./bin/learncards my_flashcards.json
+```
+
+### Via NPM
+```bash
+npm install -g learncards
+learncards my_flashcards.json
+```
+
+## Flashcards
+A Flashcard json file *MUST* contain a `cards` property where each card is
+an object which *MUST* contain a `question` and `answer` property.
+
+An optional top level property of `title` can be provided which sets the
+title in the html site accordingly.
+
+### Example
+```javascript
+{
+  "title": "My Flashcards",
+  "cards": {
+    {
+      "question": "Who is the greatest?",
+      "answer": "Brett Langdon"
+    },
+    {
+      "question": "Who is terrible at naming projects?",
+      "answer": "Brett Langdon"
+    }
+  }
+}
+```
+
+## Running
+There is a binary file included with the installation `learncards`.
+If `learncards` is installed globally then the command should be available.
+Otherwise you can run the binary directly from the `learncards` directory.
+
+When running you must provide the location of the flashcards json file.
+
+### Example
+```bash
+learncards my_flashcards.json
+```
+
+## License
+```
+The MIT License (MIT)
+
+Copyright (c) 2013 Brett Langdon <brett@blangdon.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+```
---
+++ public/app.js
@@ -0,0 +1,113 @@
+var factor = 4;
+var next_index = 0;
+var swap = false;
+
+var resize = function(name){
+    var node = document.getElementById(name);
+    node.style.width = window.innerWidth + "px";
+    node.style.height = window.innerHeight + "px";
+    node.childNodes[0].style["font-size"] = Math.round(window.innerHeight / factor) + "px";
+    node.childNodes[0].style["margin-top"] = Math.round(-node.childNodes[0].clientHeight / 2) + "px";
+};
+
+var resize_buttons = function(){
+    var new_height = Math.round(window.innerHeight / (factor * 3));
+    var middle = window.innerWidth / 2;
+
+    var options = document.getElementById("options");
+    options.style["font-size"] = Math.round(new_height / 2) + "px";
+    options.style.left = Math.round(middle - (options.clientWidth / 2)) + "px";
+};
+
+var toggle = function(name, visibility){
+    var node = document.getElementById(name);
+    if(visibility === undefined){
+        visibility = node.style.display === "none" ? "block" : "none";
+    }
+
+    node.style.display = visibility;
+    resize(name);
+};
+
+var set_text = function(name, text){
+    var node = document.getElementById(name);
+    node.childNodes[0].innerText = text;
+    resize(name);
+};
+
+var next_card = function(random){
+    random = random || false;
+    var req = new XMLHttpRequest();
+    var url = "/card.json?next=" + next_index;
+    if(random){
+        url = "/card.json?rand=true";
+    }
+    url += "&swap=" + swap;
+    req.open("GET", url, true);
+    req.onreadystatechange = function () {
+        if(req.readyState != 4 || req.status != 200) return;
+        var response = JSON.parse(req.responseText);
+        toggle("question", "block");
+        toggle("answer", "none");
+        set_text("question", response.card.question);
+        set_text("answer", response.card.answer);
+        next_index = response.index;
+    };
+    req.send();
+};
+
+
+window.onresize = function(){
+    resize("question");
+    resize("answer");
+    resize_buttons();
+}
+window.onresize();
+next_card();
+
+$(document).click(function(){
+    toggle("question");
+    toggle("answer");
+});
+
+$("#next").click(function(evt){
+    evt.stopPropagation();
+    next_index += 1;
+    next_card();
+});
+
+$("#last").click(function(evt){
+    evt.stopPropagation();
+    next_index -= 1;
+    if(next_index < 0){
+        next_index = 0;
+    }
+    next_card();
+});
+
+$("#random").click(function(evt){
+    evt.stopPropagation();
+    next_card(true);
+});
+
+$("#swap").click(function(evt){
+    evt.stopPropagation();
+    var self = $(this);
+    if(self.hasClass("normal")){
+        swap = true;
+        self.removeClass("normal");
+        self.addClass("swap");
+        self.text("Swap");
+    } else if(self.hasClass("swap")){
+        swap = "random";
+        self.removeClass("swap");
+        self.addClass("random");
+        self.text("Random");
+    } else{
+        swap = false;
+        self.removeClass("swap");
+        self.removeClass("random");
+        self.addClass("normal");
+        self.text("Normal");
+    }
+});
